c04a65abe57ef1afec505398ed6ae501fb17a2e8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-04-13 07:16:40 +0200

    hot-module fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // DO NOT EDIT
 //
 let app = require('./simple-javascript');
-module.hot && module.hot.accept('./simple-javascript', render);
+module.hot && module.hot.accept('./simple-javascript', app.main);
 if(require.main === module) app.main();
